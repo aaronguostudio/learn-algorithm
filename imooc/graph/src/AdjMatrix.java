f784e74828da358c4d5b05455a4f8c0a04f35afb
aaronguostudio <aaronguostudio@gmail.com>
2019-09-19 07:36:39 -0600

    init

---
+++ imooc/graph/src/AdjMatrix.java
@@ -0,0 +1,14 @@
+public class AdjMatrix {
+    private int V;
+    private int E;
+
+    private int[][] adj;
+
+    public AdjMatrix (String filename) {
+
+    }
+
+    public static void main (String[] args) {
+        AdjMatrix adjMatrix = new AdjMatrix("g.txt");
+    }
+}
