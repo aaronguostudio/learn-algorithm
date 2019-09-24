8f52ce8639f114f1799f3b4a93c15db5b8d1c449
Aaron Guo <aaronguostudio@gmail.com>
2019-09-24 00:01:05 -0600

    update adjactcy matrix

--- imooc/graph/src/AdjMatrix.java
+++ imooc/graph/src/AdjMatrix.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AdjMatrix {
@@ -10,13 +11,29 @@ public class AdjMatrix {
     public AdjMatrix (String filename) {
         File file = new File(filename);
         try(Scanner scanner = new Scanner(file)) {
+
             V = scanner.nextInt();
+            if (V < 0)
+                throw new IllegalArgumentException("V must be non-negative");
+
             adj = new int[V][V];
 
             E = scanner.nextInt();
+            if (E < 0)
+                throw new IllegalArgumentException("E must be non-negative");
+
             for (int i = 0; i < E; i++) {
                 int a = scanner.nextInt();
+                validateVertex(a);
+
                 int b = scanner.nextInt();
+                validateVertex(b);
+
+                if (a == b)
+                    throw new IllegalArgumentException("Self loop is detected");
+
+                if (adj[a][b] == 1)
+                    throw new IllegalArgumentException("Parallel edges are detected");
 
                 // 无向图，a b 有边意味着 b a 也有边，矩阵是可以沿对角线对折的
                 adj[a][b] = 1;
@@ -27,6 +44,40 @@ public class AdjMatrix {
             // Java will automatic class the resource if exception happens
             e.printStackTrace();
         }
+    }
+
+    public int V () {
+        return V;
+    }
+
+    public int E () {
+        return E;
+    }
+
+    public boolean hasEdge (int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        return adj[v][w] == 1;
+    }
+
+    // 找到所有的临边
+    public ArrayList<Integer> adj (int v) {
+        validateVertex(v);
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            if ( adj[v][i] == 1 )
+                res.add(i);
+        }
+        return res;
+    }
+
+    public int degree (int v) {
+        return adj(v).size();
+    }
+
+    private void validateVertex (int v) {
+        if ( v < 0 || v >= V )
+            throw new IllegalArgumentException("Vertex " + v + " is invalid");
     }
 
     @Override
